58d9b706324fc41d2f1f1450be7ccc38b7107941
TimDeng <tim_deng@linkwish.com>
2020-09-24 11:43:39 +0800

    Updates

---
+++ precache-manifest.78f5afa3cb4d47f5c7b39b750238f6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1913b731b528e307208bd8f5f04d2a75",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "e78ed0d4acbe10ea596c",
+    "url": "/test/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "5d355bc15fd798879b27",
+    "url": "/test/static/js/2.992619e1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.992619e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e78ed0d4acbe10ea596c",
+    "url": "/test/static/js/main.4e3da902.chunk.js"
+  },
+  {
+    "revision": "0f260b3071c29759b38e",
+    "url": "/test/static/js/runtime-main.84f36a5d.js"
+  }
+]);
